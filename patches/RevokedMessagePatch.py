567785701d59a50630a9c29d6a76cfb5b80fa681
Schwartzblat <alon.ponch@gmail.com>
2023-02-01 23:21:54 +0200

    New feature- keep revoked message

---
+++ patches/RevokedMessagePatch.py
@@ -0,0 +1,26 @@
+from patches.Patch import Patch
+import re
+
+
+class RevokedMessagePatch(Patch):
+
+    REVOKE_MESSAGE_RE = re.compile('\.method (?:\w+ )*\w+\(LX\/\w+;Z\)V\s*\.\w+ \w+\s*(.*?)\.end method', re.DOTALL)
+    REVOKE_MESSAGE_REPLACE = """
+    if-eqz p2, :cond_self_delete
+    return-void
+    :cond_self_delete
+    """
+    def __init__(self, extracted_path):
+        super().__init__(extracted_path)
+        self.print_message = (
+            "[+] Patching revoke message method..."
+        )
+
+    def class_filter(self, class_data: str) -> bool:
+        if '"msgstore/edit/revoke "' in class_data:
+            return True
+        return False
+
+    def class_modifier(self, class_data) -> str:
+        revoke_function_data = self.REVOKE_MESSAGE_RE.findall(class_data)[0]
+        return class_data.replace(revoke_function_data, self.REVOKE_MESSAGE_REPLACE+revoke_function_data)
