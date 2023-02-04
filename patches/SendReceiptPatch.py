c372b377e6c8e543638cbf0d2a4f2b47ac159710
Schwartzblat <alon.ponch@gmail.com>
2023-02-04 20:49:46 +0200

    New feature: disable read and message received receipts

---
+++ patches/SendReceiptPatch.py
@@ -0,0 +1,28 @@
+from patches.Patch import Patch
+import re
+
+
+class RevokedMessagePatch(Patch):
+    RECEIPT_METHOD_RE = re.compile(
+        "\.method public constructor <init>\(.*?Z\)V\s*\.\w+ \w+\s*(.*?)\.end method",
+        re.DOTALL,
+    )
+    RECEIPT_METHOD_REPLACE = """
+    const p9, 0x1
+    """
+
+    def __init__(self, extracted_path):
+        super().__init__(extracted_path)
+        self.print_message = "[+] Patching send read receipt method..."
+
+    def class_filter(self, class_data: str) -> bool:
+        if '"; shouldForceReadSelfReceipt="' in class_data:
+            return True
+        return False
+
+    def class_modifier(self, class_data) -> str:
+        send_receipt_constructor_body = self.RECEIPT_METHOD_RE.findall(class_data)[0]
+        return class_data.replace(
+            send_receipt_constructor_body,
+            self.RECEIPT_METHOD_REPLACE + send_receipt_constructor_body,
+        )
