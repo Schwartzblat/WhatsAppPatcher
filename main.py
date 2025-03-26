971ef1d295f56566c2ef256f4857b55e49d1c0f5
alon <alon.ponch@gmail.com>
2025-03-27 00:57:10 +0200

    Add downloader

--- main.py
+++ main.py
@@ -6,24 +6,20 @@ from ultimate_patcher import config
 from ultimate_patcher.apk_utils import extract_apk, compile_apk, sign_apk
 from artifactory import prepare_artifactory
 from ultimate_patcher.patcher import patch_apk
+from downloader import download_latest_whatsapp
 
 
 def get_args():
     parser = argparse.ArgumentParser(description='')
-    parser.add_argument('-p', '--apk-path', dest='apk_path', help='APK path', required=True)
+    parser.add_argument('-p', '--apk-path', dest='apk_path', help='APK path', required=False, default='latest')
     parser.add_argument('-o', '--output', dest='output', help='Output APK path', required=False, default='output.apk')
     parser.add_argument('-t', '--temp', dest='temp_path', help='Temp path for extracted content', required=False,
                         default='./temp')
     parser.add_argument('--arch', dest='arch', help='Architecture', required=False, default='arm64-v8a',
                         choices=['arm64-v8a', 'armeabi-v7a', 'x86', 'x86_64'])
-    parser.add_argument('--artifactory', dest='artifactory', help='Artifactory path', required=False, default='./artifactory.json')
+    parser.add_argument('--artifactory', dest='artifactory', help='Artifactory path', required=False,
+                        default='./artifactory.json')
     return parser.parse_args()
-
-
-def check_args(args) -> None:
-    if not os.path.exists(args.apk_path):
-        print('APK file not found')
-        exit(1)
 
 
 def clean_up(args):
@@ -31,9 +27,11 @@ def clean_up(args):
     if os.path.exists(args.artifactory):
         os.remove(args.artifactory)
 
+
 def main():
     args = get_args()
-    check_args(args)
+    if args.apk_path == 'latest':
+        args.apk_path = download_latest_whatsapp('WhatsApp-Latest.apk')
     try:
         print('[+] Extracting APK...')
         extract_apk(args.apk_path, str(pathlib.Path(args.temp_path) / config.EXTRACTED_TEMP_DIR))
